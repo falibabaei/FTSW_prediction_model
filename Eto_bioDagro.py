293379f5dcc09d70921332a671aa81e1c39ae8c6
falibabaei <66964597+falibabaei@users.noreply.github.com>
2022-07-19 11:21:39 +0100

    Update Eto_bioDagro.py

--- Eto_bioDagro.py
+++ Eto_bioDagro.py
@@ -19,15 +19,7 @@ register_matplotlib_converters()
 
 
 
-#hyperparameters
-seq_len=7 #(number of days for predicting ET)
-Batch_size=63#50#40 #128
-Epoch=500
-lr= 0.00089#0.00025856#0.0003968 #2e-3
-decay= 0.00094#0.0003011 #0.0003456#1e-3
-hidden_units=38 #number of hidden neurons in LSTM layer 32,64,128,256,
-dropout_size=0.09313# 0.17521#0.09313
-n_out=1 
+
 NAME = f"swc1_diario_biodagro-{int(time.time())}.h5"
 
 dir_='D:/SM_estimation_paper'
