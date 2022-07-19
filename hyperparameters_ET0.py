9e1d69141c4f525611d3b840bebbe4d3071c3dfe
falibabaei <66964597+falibabaei@users.noreply.github.com>
2022-07-19 11:13:04 +0100

    Update hyperparameters_ET0.py

--- hyperparameters_ET0.py
+++ hyperparameters_ET0.py
@@ -8,4 +8,5 @@ lr= 0.00089#0.00025856#0.0003968 #2e-3
 decay= 0.00094#0.0003011 #0.0003456#1e-3
 hidden_units=38 #number of hidden neurons in LSTM layer 32,64,128,256,
 dropout_size=0.09313# 0.17521#0.09313
+n_layers=2#number of BiLSTM layers in model
 
